ce362ead7f0d95b8a7e07b45b28ab812532b9228
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-08-23 13:59:58 +0200

    pizza menu components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1>Nasr's italian pizza co.</h1>
+  )
+}
+
+export default Header
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Pizza() {
+  return (
+    <div>
+        <img src="https://st3.idealista.it/cms/archivie/styles/idcms_large_desktop/public/2019-05/media/image/margherita_wikipedia.jpg?fv=bOcWknfe&itok=9WM7SPJF" alt="" />
+        <h2>Margherita</h2>
+        <p>As well as mozzarella, the other ingredients are tomato, oil and basil,</p>
+    </div>
+  )
+}
+
+export default Pizza
